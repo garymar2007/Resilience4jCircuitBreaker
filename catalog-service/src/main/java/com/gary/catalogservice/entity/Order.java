297636502d7e6388d3f7ea576c48c142a91f12ed
Gary Ma <gary.ma@csgi.com>
2023-04-17 22:08:11 +0200

    first commit

---
+++ catalog-service/src/main/java/com/gary/catalogservice/entity/Order.java
@@ -0,0 +1,33 @@
+package com.gary.catalogservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "ORDERS_TBL")
+public class Order {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String category;
+    private String color;
+    private double price;
+
+    public Order(String name, String category, String color, double price) {
+        this.name = name;
+        this.category = category;
+        this.color = color;
+        this.price = price;
+    }
+}
---
+++ catalog-service/src/main/java/com/gary/catalogservice/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.gary.catalogservice.repository;
+
+import com.gary.catalogservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByCategory(String category);
+}
